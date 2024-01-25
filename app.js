ee7fb6dff2adfdafe4a88c1d948dd741ca5be01b
crazyshinobi <chetansingh410@gmail.com>
2024-01-25 16:47:28 +0530

    Fixed mobile menu

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const menuIcon = document.querySelector(".menu");
 let flag = 0;
 
 menuIcon.addEventListener("click", function () {
-
   if (flag == 0) {
     mobileNavbar.style.display = "flex";
     menuIcon.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
@@ -16,17 +15,28 @@ menuIcon.addEventListener("click", function () {
   }
 });
 
+function closeMenu() {
+  mobileNavbar.style.display = "none";
+  menuIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+}
+
+// Add event listener for each link in mobile navbar
+const mobileNavLinks = document.querySelectorAll('.mobile-navbar li');
+mobileNavLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
+
 
 function downloadPDF() {
   // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
-  var pdfPath = './resume/Chetan Singh NextLevel Resume (1).pdf';
-  
+  var pdfPath = "./resume/Chetan Singh NextLevel Resume (1).pdf";
+
   // Create a link element
-  var link = document.createElement('a');
+  var link = document.createElement("a");
   link.href = pdfPath;
 
   // Set the download attribute to specify the file name
-  link.download = 'document.pdf';
+  link.download = "document.pdf";
 
   // Append the link to the body
   document.body.appendChild(link);
